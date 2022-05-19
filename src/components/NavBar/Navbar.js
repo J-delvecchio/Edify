3ab825552288cbc79ab6540fb3ddf4353e32f281
Joaquin Del Vecchio <joaquindelvecchio@Joaquins-MacBook-Air.local>
2022-05-19 20:13:14 -0300

    header

---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,23 @@
+export const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <h1>Edify</h1>
+        <ul className="navLinks">
+          <li>
+            <a href="#a">HOME</a>
+          </li>
+          <li>
+            <a href="#a">CLOTHES</a>
+          </li>
+          <li>
+            <a href="#a">ABOUT US</a>
+          </li>
+          <li>
+            <a href="#a">CONTACT</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
