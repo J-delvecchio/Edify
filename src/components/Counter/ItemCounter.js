b60009e4c0678a9201008b1502e5a6240da8d369
Joaquin Del Vecchio <joaquindelvecchio@Joaquins-MacBook-Air.local>
2022-05-30 20:23:48 -0300

    agregarPedido

---
+++ src/components/Counter/ItemCounter.js
@@ -0,0 +1,42 @@
+import "./itemCounter.scss"
+import { useState } from "react";
+
+const remeras = {
+    rojas:20
+}
+
+export const ItemCounter = () =>{
+    let [contador, setContador] = useState(1)
+
+
+    const incrementar = () =>{
+
+        remeras.rojas > contador? setContador(contador+1) : setContador(contador)
+    }
+
+    const decrementar = () =>{
+
+        contador > 0? setContador(contador-1) : setContador(contador)
+    }
+
+    const resetear = () =>{
+
+        setContador(0)
+    }
+
+
+
+
+    return(
+        <div className="container border w-25 agregarPedido" >
+            <div>
+                <h2 className="producto">Remera roja</h2>
+                <hr/>
+                <button className="btn btn-primary m-2 agregarPedido p-4" onClick={incrementar}>+</button>
+                <span className="p-5">{contador}</span>
+                <button className="btn btn-primary m-2 agregarPedido p-4" onClick={decrementar}>-</button>
+            </div>
+            <button className="btn btn-primary p-5 m-2 w-75 fs-4" onClick={resetear}>Agregar al carrito</button>
+        </div>
+    )
+}
